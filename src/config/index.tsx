9e6d6fd0813849124bdb5c8771fb8f7180eb8858
James333 | zkLink Labs <40433945+ggg223399@users.noreply.github.com>
2025-04-15 20:19:07 +0800

    Update index.tsx

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -4,7 +4,7 @@ import { SolanaAdapter } from '@reown/appkit-adapter-solana/react'
 
 
 // Get projectId from https://cloud.reown.com
-export const projectId = import.meta.env.VITE_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
+export const projectId = "b68a500fc29ff979e1a239b78320e6c6" // this is a public projectId only to use on localhost
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
